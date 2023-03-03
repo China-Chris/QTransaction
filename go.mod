9b5e90caf096bd56300deddd679ea5074838e96a
Ed <40153785+China-Chris@users.noreply.github.com>
2023-03-03 10:53:47 +0800

    update: 更新支付进行实盘交易

--- go.mod
+++ go.mod
@@ -5,35 +5,35 @@ go 1.18
 require (
 	github.com/beego/beego/v2 v2.0.7
 	github.com/gorilla/websocket v1.5.0
+	github.com/parnurzeal/gorequest v0.2.16
+	github.com/shopspring/decimal v1.3.1
 	github.com/spf13/viper v1.15.0
 	gorm.io/driver/mysql v1.4.7
 	gorm.io/gorm v1.24.5
 )
 
 require (
-	github.com/adshao/go-binance/v2 v2.4.1 // indirect
-	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	moul.io/http2curl v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-	"github.com/adshao/go-binance/v2"
 	"github.com/gorilla/websocket"
+	"github.com/parnurzeal/gorequest"
 	"github.com/shopspring/decimal"
 	"log"
 	"strconv"
 	"strings"
+	"time"
 )
 
-type Account struct {
-	Balance float64 // 账户余额
-	Holding float64 // 持仓
+var url = "https://api4.binance.com"
+var key = "yCij8LdqY0u7boPzB4GN9ACzMAaqaS4L1ezUPwGCUHaQmUqjYQxc6mZM6xm1WQDw"
+var secret = "7TuigXuGSdRn6URlefpyVC71M8vFf1ybxcOWQBnQkVOUsXdiPlYqFZZrfD0QRVou"
+
+type AccountAmount struct {
+	Balances []Amount
 }
 
-type Candle struct {
-	Time   int64
-	Open   float64
-	High   float64
-	Low    float64
-	Close  float64
-	Volume float64
-}
-
-func init() {
-	//config.ParseConfig("./configs/cfg.yml")
-	//logger.InitLog()
+// Amount 账户
+type Amount struct {
+	Asset  string `json:"asset"`
+	Free   string `json:"free"`
+	Locked string `json:"locked"`
 }
 
 // 移动平均线
@@ -42,7 +41,6 @@ func (ma *MovingAverage) Add(val float64) {
 		fmt.Println(val)
 		return
 	}
-
 	// 移除队头数据
 	ma.Data = append(ma.Data[1:], val)
 }
@@ -58,20 +56,7 @@ func (ma *MovingAverage) Value() float64 {
 	return sum / float64(ma.Period)
 }
 
-func btc() {
-	var (
-		apiKey    = "G30kZdFVnK2zugrWmw91lZdqDWzjsiLHY7eAm10IWaV2rc5Uq6LE7eBSE2J9NKK9Copy"
-		secretKey = "OVNWYgvbVeQ6D7p1iCyQV3AvFVgolLiOacFspQikcSAl211i1JoMqTe2rt6aoqZg"
-	)
-	//创建客户端
-	client := binance.NewClient(apiKey, secretKey)
-
-	// 定义交易货币对和交易参数
-	//symbol := "BTCUSDT"
-	//tradePercent := 0.1 // 每次交易使用不超过10%的资金
-	//futuresClient := binance.NewFuturesClient(apiKey, secretKey)   // USDT-M Futures
-	//deliveryClient := binance.NewDeliveryClient(apiKey, secretKey) // Coin-M Futures
-
+func QT() {
 	// 建立 WebSocket 连接
 	wsURL := "wss://stream.binancefuture.com/ws/btcusdt@ticker"
 	c, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
@@ -79,12 +64,9 @@ func btc() {
 		log.Fatal("dial error:", err)
 	}
 	defer c.Close()
-
 	//初始化移动平均线
 	ma := MovingAverage{Period: 50}
-	// 初始化账户
-	account := Account{Balance: 1000}
-	// 接收 WebSocket 数据
+	pos := 0
 	for {
 		_, message, err := c.ReadMessage()
 		if err != nil {
@@ -102,7 +84,7 @@ func btc() {
 			fmt.Println("Error: unable to extract C field value")
 			return
 		}
-		sValue := jsonStr[sIndex+4 : endSIndex]
+		//sValue := jsonStr[sIndex+4 : endSIndex]
 		//获取C字段位置
 		CIndex := strings.Index(jsonStr, `"c":`)
 		if CIndex == -1 {
@@ -122,77 +104,105 @@ func btc() {
 		// 添加到移动平均线中
 		ma.Add(price)
 		// 获取移动平均线值
+		min := decimal.NewFromFloat(0.0001)
 		maValue := ma.Value()
 		if maValue != 0 {
-			if price < maValue { //如果小于均值买入
-				fmt.Println("买入信号")
-				//计算买入数量
-				availableMr := account.Balance
-				quantityMr := decimal.NewFromFloat(availableMr).Div(decimal.NewFromFloat(price)).Floor()
-				//执行买入操作
-				order, err := client.NewCreateOrderService().Symbol("BTCUSDT").
-					Side(binance.SideTypeBuy).Type(binance.OrderTypeLimit).
-					TimeInForce(binance.TimeInForceTypeGTC).Quantity(quantityMr.String()).
-					Price(fmt.Sprintf("%.8f", price)).Do(context.Background())
-				if err != nil {
-					fmt.Println(err)
-					return
+			if price > maValue { //如果小于均值买入
+				pos += 1
+				if pos == 5 {
+					pos = 0
+					fmt.Println("买入信号")
+					usdt := GetUsdt()
+					if usdt.LessThanOrEqual(decimal.NewFromInt(10)) {
+						fmt.Println("usdt 额度不足")
+						continue
+					}
+					err := Order("BUY", usdt.Div(decimal.NewFromFloat(price)).Sub(min).StringFixedBank(5))
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					fmt.Println("买入成功:" + usdt.String())
 				}
-				fmt.Println(order)
-				// 计算可用余额
-				available := account.Balance / price //计算持仓价值
-				// 更新账户余额和持仓
-				account.Balance = 0          //余额归零
-				account.Holding += available //持仓更新
-				fmt.Printf("S value: %s,C value: %s,  ma value: %v\n", sValue, cValue, maValue)
-				fmt.Printf("账户余额: %.2f, 持仓价值: %.2f, 盈亏: %.2f\n", account.Balance, account.Holding*price, account.Balance+account.Holding*price-10)
-
-			} else if price > maValue { //如果价格大于均值卖出
-				if account.Holding != 0 {
+			} else if price < maValue { //如果价格大于均值卖出
+				pos -= 1
+				if pos == -5 {
+					pos = 0
 					fmt.Println("卖出信号")
-					// 调用API获取账户信息
-					accountBNB, err := client.NewGetAccountService().Do(context.Background())
+					btc := GetBtc()
+					if btc.LessThanOrEqual(decimal.NewFromFloat(0.0001)) {
+						fmt.Println("btc 额度不足")
+						continue
+					}
+					err := Order("SELL", btc.Sub(decimal.NewFromFloat(0.00001)).StringFixed(5))
 					if err != nil {
 						fmt.Println(err)
-						return
-					}
-					// 获取账户中持有的BTC数量
-					var holdingBtc decimal.Decimal
-					for _, b := range accountBNB.Balances {
-						if b.Asset == "BTC" {
-							holdingBtc, _ = decimal.NewFromString(b.Free)
-							break
-						}
-					}
-					quantityDec := holdingBtc.Mul(decimal.NewFromFloat(1)) //
-					quantity, _ := quantityDec.Round(6).Float64()          // 转换为float64类型，保留6位小数
+						continue
 
-					order, err := client.NewCreateOrderService().Symbol("BTCUSDT").
-						Side(binance.SideTypeSell).Type(binance.OrderTypeLimit).
-						TimeInForce(binance.TimeInForceTypeGTC).Quantity(decimal.NewFromFloat(quantity).String()).
-						Price(fmt.Sprintf("%.8f", price)).Do(context.Background())
-					if err != nil {
-						fmt.Println(err)
-						return
 					}
-					fmt.Println(order)
-					// 计算可用余额
-					available := account.Holding * price //计算收益价值
-					// 更新账户余额和持仓
-					account.Holding = 0          //持仓更新
-					account.Balance += available //余额更新
-					fmt.Printf("S value: %s,C value: %s,  ma value: %v\n", sValue, cValue, maValue)
-					fmt.Printf("账户余额: %.2f, 持仓价值: %.2f, 盈亏: %.2f\n", account.Balance, account.Holding*price, account.Balance+account.Holding*price-10)
+					fmt.Println("卖出成功:" + btc.String())
 				}
 			}
 		}
 	}
 }
 
-//func floatToString(f float64) string {
-//	return strconv.FormatFloat(f, 'f', -1, 64)
-//}
-
 func main() {
-	btc()
+	QT()
+}
+
+func GetUsdt() decimal.Decimal {
+	balance := GetBalance()
+	for _, v := range balance {
+		if v.Asset == "USDT" {
+			usdt, _ := decimal.NewFromString(v.Free)
+			return usdt
+		}
+	}
+	return decimal.Zero
+}
+
+func GetBtc() decimal.Decimal {
+	balance := GetBalance()
+	for _, v := range balance {
+		if v.Asset == "BTC" {
+			usdt, _ := decimal.NewFromString(v.Free)
+			return usdt
+		}
+	}
+	return decimal.Zero
+}
+
+func GetBalance() []Amount {
+	timestamp := fmt.Sprintf("%d", time.Now().UnixMilli())
+	signature := hmacSha256("timestamp="+timestamp, secret)
+	rsp := AccountAmount{}
+	_, _, errs := gorequest.New().Get(url+"/api/v3/account?timestamp="+timestamp+"&signature="+signature).AppendHeader("X-MBX-APIKEY", key).EndStruct(&rsp)
+	if len(errs) != 0 {
+		fmt.Println(errs)
+		return nil
+	}
+	return rsp.Balances
+}
+
+// Order
+func Order(side, amount string) error {
+	parameter := fmt.Sprintf("quantity=%s&side=%s&symbol=BTCUSDT&timestamp=%d&type=MARKET", amount, side, time.Now().UnixMilli())
+	signature := hmacSha256(parameter, secret)
+	parameter = parameter + "&signature=" + signature
+	_, s, errs := gorequest.New().Post(url+"/api/v3/order"). //+parameter+"&signature="+signature
+		SendString(parameter).
+		AppendHeader("X-MBX-APIKEY", key).End()
+	if len(errs) != 0 {
+		fmt.Println(errs)
+		return errs[0]
+	}
+	fmt.Println(s)
+	return nil
+}
+
+func hmacSha256(data string, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
 }
